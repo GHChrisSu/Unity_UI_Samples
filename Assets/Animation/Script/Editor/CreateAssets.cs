5f8d9344faaf904233274720d0b089c74023f579
ChrisSu <suhao@espacetime.com>
2017-11-08 19:12:17 +0800

    Create assets, editor script

---
+++ Assets/Animation/Script/Editor/CreateAssets.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+public class CreateAssets
+{
+    [MenuItem("Assets/Create assets")]
+    public static void CreateAsset()
+    {
+        ScriptableObjectUtility.CreateAsset<AnimatorParameter>();
+    }
+}
